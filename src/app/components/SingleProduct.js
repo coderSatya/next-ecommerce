5d659955c9a6636555345d9d40ebcf31cff93b92
Satya Prakash <satya.prakash@webskitters.in>
2023-10-19 20:15:20 +0530

    added single product

--- src/app/components/SingleProduct.js
+++ src/app/components/SingleProduct.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
+import FormattedPrice from './FormattedPrice';
 
 const SingleProduct = ({ProductDetailItem}) => {
    console.log(ProductDetailItem.title, 'ProductDetailItem')
   return (
     <div>
+    <div className='grid lg:grid-cols-2 gap-5 bg-white p-4 rounded-lg'>
    
-   <Image src={ProductDetailItem.image} width={500} height={500}/>
+   <Image src={ProductDetailItem.image} width={500} height={500} className='w-full max-h-[700px] object-cover rounded-lg'/>
     </div>
+    <div>
+    <p>{ProductDetailItem.title}</p>
+    <p><FormattedPrice amount={ProductDetailItem.price} /></p>
+    </div>
+    </div>
+
   )
 }
 
